5c995ffb4fd02b7626f97f62e61370f6cbf092d1
sanaa <Sanaakumar07@gmail.com>
2025-09-04 16:45:37 +0530

    pattern

--- diamond/main.c
+++ diamond/main.c
@@ -1,14 +1,26 @@
-//
-//  main.c
-//  diamond
-//
-//  Created by Sanaa Kumar on 04/09/25.
-//
-
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+#include<stdio.h>
+int main (void){
+    int i=1,j=1;
+    for (i=1;i<=10;i++)
+    {
+        for(j=1;j<=9;j++){
+            printf("");
+        }
+        for(j=1;j<=i*2-1;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (i=9;i>0;i--)
+    {
+        for(j=1;j<=9;j++){
+            printf("");
+        }
+        for(j=1;j<=i*2-1;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    
 }
+
